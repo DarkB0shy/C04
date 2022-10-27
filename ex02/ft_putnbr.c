3c7b8b843396313c3dd18e17d2a7f6b12de85164
Davide Carassiti <dcarassi@e3r4p4.42roma.it>
2022-10-27 11:34:40 +0200

    finoaex02

--- ex02/ft_putnbr.c
+++ ex02/ft_putnbr.c
@@ -1,23 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcarassi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/26 20:32:52 by dcarassi          #+#    #+#             */
-/*   Updated: 2022/10/27 10:28:10 by dcarassi         ###   ########.fr       */
+/*   Created: 2022/10/27 10:29:00 by dcarassi          #+#    #+#             */
+/*   Updated: 2022/10/27 11:33:10 by dcarassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	ft_strlen(char *str)
+void	ft_putnbr(int nb)
 {
-	int	i;
+	char c;
 
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
+	if (nb == -2147483648)
+		write(1, "-2147483648", 11);	
+	else if (nb < 0)
+	{
+		nb = -nb;
+		write(1, "-", 1);
+		ft_putnbr(nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		c = nb + '0';
+		write(1, &c, 1);
+	}
+}
+int	main(void)
+{
+	ft_putnbr(-2147483648);
+	return (0);
 }
